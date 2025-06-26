2c8e8f5bb7457d2d12296eebd35fa736a5b1fa21
KareemAbuayyash <202202464@bethlehem.edu>
2025-06-26 22:23:52 +0300

    Add 114 Surah JSON files (clean text + diacritics)

--- assets/quran/surahs/index.js
+++ assets/quran/surahs/index.js
@@ -1,9 +1,232 @@
 // assets/quran/surahs/index.js
 import s1 from './1.json';
 import s2 from './2.json';
-// … استورد كل ملف سورة بنفس الصيغة حتى 114.json
+import s3 from './3.json';
+import s4 from './4.json';
+import s5 from './5.json';
+import s6 from './6.json';
+import s7 from './7.json';
+import s8 from './8.json';
+import s9 from './9.json';
+import s10 from './10.json';
+import s11 from './11.json';
+import s12 from './12.json';
+import s13 from './13.json';
+import s14 from './14.json';
+import s15 from './15.json';
+import s16 from './16.json';
+import s17 from './17.json';
+import s18 from './18.json';
+import s19 from './19.json';
+import s20 from './20.json';
+import s21 from './21.json';
+import s22 from './22.json';
+import s23 from './23.json';
+import s24 from './24.json';
+import s25 from './25.json';
+import s26 from './26.json';
+import s27 from './27.json';
+import s28 from './28.json';
+import s29 from './29.json';
+import s30 from './30.json';
+import s31 from './31.json';
+import s32 from './32.json';
+import s33 from './33.json';
+import s34 from './34.json';
+import s35 from './35.json';
+import s36 from './36.json';
+import s37 from './37.json';
+import s38 from './38.json';
+import s39 from './39.json';
+import s40 from './40.json';
+import s41 from './41.json';
+import s42 from './42.json';
+import s43 from './43.json';
+import s44 from './44.json';
+import s45 from './45.json';
+import s46 from './46.json';
+import s47 from './47.json';
+import s48 from './48.json';
+import s49 from './49.json';
+import s50 from './50.json';
+import s51 from './51.json';
+import s52 from './52.json';
+import s53 from './53.json';
+import s54 from './54.json';
+import s55 from './55.json';
+import s56 from './56.json';
+import s57 from './57.json';
+import s58 from './58.json';
+import s59 from './59.json';
+import s60 from './60.json';
+import s61 from './61.json';
+import s62 from './62.json';
+import s63 from './63.json';
+import s64 from './64.json';
+import s65 from './65.json';
+import s66 from './66.json';
+import s67 from './67.json';
+import s68 from './68.json';
+import s69 from './69.json';
+import s70 from './70.json';
+import s71 from './71.json';
+import s72 from './72.json';
+import s73 from './73.json';
+import s74 from './74.json';
+import s75 from './75.json';
+import s76 from './76.json';
+import s77 from './77.json';
+import s78 from './78.json';
+import s79 from './79.json';
+import s80 from './80.json';
+import s81 from './81.json';
+import s82 from './82.json';
+import s83 from './83.json';
+import s84 from './84.json';
+import s85 from './85.json';
+import s86 from './86.json';
+import s87 from './87.json';
+import s88 from './88.json';
+import s89 from './89.json';
+import s90 from './90.json';
+import s91 from './91.json';
+import s92 from './92.json';
+import s93 from './93.json';
+import s94 from './94.json';
+import s95 from './95.json';
+import s96 from './96.json';
+import s97 from './97.json';
+import s98 from './98.json';
+import s99 from './99.json';
+import s100 from './100.json';
+import s101 from './101.json';
+import s102 from './102.json';
+import s103 from './103.json';
+import s104 from './104.json';
+import s105 from './105.json';
+import s106 from './106.json';
+import s107 from './107.json';
+import s108 from './108.json';
+import s109 from './109.json';
+import s110 from './110.json';
+import s111 from './111.json';
+import s112 from './112.json';
+import s113 from './113.json';
+import s114 from './114.json';
 
 export default {
   1: s1,
-  2: s2
+  2: s2,
+  3: s3,
+  4: s4,
+  5: s5,
+  6: s6,
+  7: s7,
+  8: s8,
+  9: s9,
+  10: s10,
+  11: s11,
+  12: s12,
+  13: s13,
+  14: s14,
+  15: s15,
+  16: s16,
+  17: s17,
+  18: s18,
+  19: s19,
+  20: s20,
+  21: s21,
+  22: s22,
+  23: s23,
+  24: s24,
+  25: s25,
+  26: s26,
+  27: s27,
+  28: s28,
+  29: s29,
+  30: s30,
+  31: s31,
+  32: s32,
+  33: s33,
+  34: s34,
+  35: s35,
+  36: s36,
+  37: s37,
+  38: s38,
+  39: s39,
+  40: s40,
+  41: s41,
+  42: s42,
+  43: s43,
+  44: s44,
+  45: s45,
+  46: s46,
+  47: s47,
+  48: s48,
+  49: s49,
+  50: s50,
+  51: s51,
+  52: s52,
+  53: s53,
+  54: s54,
+  55: s55,
+  56: s56,
+  57: s57,
+  58: s58,
+  59: s59,
+  60: s60,
+  61: s61,
+  62: s62,
+  63: s63,
+  64: s64,
+  65: s65,
+  66: s66,
+  67: s67,
+  68: s68,
+  69: s69,
+  70: s70,
+  71: s71,
+  72: s72,
+  73: s73,
+  74: s74,
+  75: s75,
+  76: s76,
+  77: s77,
+  78: s78,
+  79: s79,
+  80: s80,
+  81: s81,
+  82: s82,
+  83: s83,
+  84: s84,
+  85: s85,
+  86: s86,
+  87: s87,
+  88: s88,
+  89: s89,
+  90: s90,
+  91: s91,
+  92: s92,
+  93: s93,
+  94: s94,
+  95: s95,
+  96: s96,
+  97: s97,
+  98: s98,
+  99: s99,
+  100: s100,
+  101: s101,
+  102: s102,
+  103: s103,
+  104: s104,
+  105: s105,
+  106: s106,
+  107: s107,
+  108: s108,
+  109: s109,
+  110: s110,
+  111: s111,
+  112: s112,
+  113: s113,
+  114: s114,
 };
