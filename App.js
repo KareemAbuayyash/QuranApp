b8c93cc40f6d3381f2ea83b811ae43111fcc464c
KareemAbuayyash <202202464@bethlehem.edu>
2025-06-28 12:42:04 +0300

    Update HomeScreen options to hide header for improved navigation experience

--- App.js
+++ App.js
@@ -30,7 +30,7 @@ export default function App() {
         <Stack.Screen 
           name="Home" 
           component={HomeScreen} 
-          options={{ title: 'الصفحة الرئيسية' }} 
+          options={{ headerShown: false }}
         />
         <Stack.Screen
           name="SurahList"
